bb7e65d4d5e79f8387d0bda24293ffae60bbfcbb
chrismatgit <christianmatabaro@gmail.com>
2020-11-30 21:32:45 +0300

    Adding reducers and actions Adding Items. Typescript Interfaces Vs Types

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { AddNewItem } from "./AddNewItem";
+import { useAppState } from "./AppStateContext";
+import { Card } from "./Card";
+import { useItemDrag } from "./useItemDrag";
 
 interface ColumnProps {
   text: string;
+  index: number;
+  id: string;
 }
 
-export const Column = ({
-  text,
-  children,
-}: React.PropsWithChildren<ColumnProps>) => {
+export const Column = ({ text, index, id }: ColumnProps) => {
+  const { state, dispatch } = useAppState();
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { drag } = useItemDrag({ type: "COLUMN", id, index, text });
+
+  drag(ref);
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref}>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
+      {state.lists[index].tasks.map((task) => (
+        <Card text={task.text} key={task.id} />
+      ))}
       <AddNewItem
         toggleButtonText="+ Add another task"
-        onAdd={console.log}
+        onAdd={(text) =>
+          dispatch({ type: "ADD_TASK", payload: { text, taskId: id } })
+        }
         dark
       />
     </ColumnContainer>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend as Backend } from "react-dnd-html5-backend";
+import { AppStateProvider } from "./AppStateContext";
 
-ReactDOM.render(<App />,document.getElementById('root'));
-
+ReactDOM.render(
+  <DndProvider backend={Backend}>
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
+  </DndProvider>,
+  document.getElementById("root")
+);
